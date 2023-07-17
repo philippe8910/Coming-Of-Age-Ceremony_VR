daf029760ac9b6e75f1e224da0c85153cca90df9
PhilippeCheng <philippe8910@gmail.com>
2023-07-17 18:27:54 +0800

    added teleport action

---
+++ Assets/LineRendererScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererScript : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform firstPos, endPos;
+    public Vector3 target;
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0 , firstPos.position);
+        lineRenderer.SetPosition(1 , endPos.position);
+    }
+
+    public void SetTarget(Vector3 _target)
+    {
+        target = _target;
+    }
+}
